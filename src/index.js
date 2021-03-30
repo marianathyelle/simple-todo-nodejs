df7dc149acfd7218a513bfbec46f973d59b42627
Maria Nathyelle <nathyellemonteiro19@gmail.com>
2021-03-30 06:43:41 -0300

    add users post

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username)
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const usernameAlreadyExists = users.some((user) => user.username === username)
+
+  if (usernameAlreadyExists) {
+    return response.status(400).json({ error: "Username not available." })
+  }
+
+  const user = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: [],
+  }
+
+  users.push(user)
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
